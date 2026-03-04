7b8ce8fde412d7ba4f17335b2209592ded2f8fc0
Chemical-COE <cfirkins@unm.edu>
2026-03-03 22:24:44 -0700

    Enhance DataFrame with fish and plants information
    Updated the DataFrame description and added fish and plants data.

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import pandas as pd
 
 st.title("Hi there! Welcome to Streamlit!")
-st.write("My first DataFrame")
+st.write("My first DataFrame... it's about fish")
 st.dataframe(
     pd.DataFrame({
-        'A': [1, 5, 9, 7],
-        'B': [3, 2, 4, 8]
+        'Fish': ['Honey Gourami', 'Black Neon Tetra', 'Pygmy Corydora', 'Cherry Shrimp'],
+        'Plants': ['Red Floater', 'Cryptocoryne', 'Monte Carlo', 'Annubias']
     })
 )
+
+
+st.write('More importantly... here is a explore my two fish pages')
