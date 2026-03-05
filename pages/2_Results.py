823da46ed24163dbe8ff0c4a6a3bdc6249a01b2f
Chemical-COE <cfirkins@unm.edu>
2026-03-04 21:36:25 -0700

    Update 2_Results.py

--- pages/2_Results.py
+++ pages/2_Results.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Hi there! Welcome to the Results page!")
+st.title("Welcome to the Results page!")
 
 sin_list = pd.read_excel('SinList_result.xlsx')
 echa_list = pd.read_excel('chemical_universe_list_en.xlsx')
@@ -16,4 +16,30 @@ if 'chemical_data' in st.session_state:
 else:
     st.warning("Please upload your data on the upload page first.")
 
+if l = 1:
+    # Normalize the sheets to have cas numbers as strings
+    echa_list['cas_number'] = echa_list['cas_number'].astype(str)
+    sin_list['cas_number'] = sin_list['cas_number'].astype(str)
+    test_list['cas_number'] = test_list['cas_number'].astype(str)
 
+    # Fill missing data with "NIES" -> Not In Excel Sheet (Missing From Excel Sheet)
+    sin_list.fillna('NIES', inplace=True)
+    echa_list.fillna('NIES', inplace=True)
+
+    # Join the test list with the refrerence lists
+    echa_results = test_list.merge(echa_list, on='cas_number', how='left')
+    sin_results = test_list.merge(sin_list, on='cas_number', how='left')
+
+    # Fill the Na Results with Safe (These can be filtered)
+    echa_results.fillna('Safe', inplace=True)
+    sin_results.fillna('Safe', inplace=True)
+
+    # Filter and seperate DataFrames
+    echa_results_safe = echa_results[echa_results['name_y'] == 'Safe'][['name_x' , 'cas_number']].copy().reset_index(drop = True)
+    echa_results_not_safe = echa_results[echa_results['name_y'] != 'Safe'].copy().reset_index(drop = True)
+
+    sin_results_safe = sin_results[sin_results['name_y'] == 'Safe'][['name_x' , 'cas_number']].copy().reset_index(drop = True)
+    sin_results_not_safe = sin_results[sin_results['name_y'] != 'Safe'].copy().reset_index(drop = True)
+
+    st.write('Your sin list results are bellow')
+    st.dataframe(sin_results_not_safe)
