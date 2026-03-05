026c4ce38e5eaf341e7cc8392605a0845f0a87a1
Chemical-COE <cfirkins@unm.edu>
2026-03-04 20:34:38 -0700

    Revise Streamlit app for chemical management
    Updated the Streamlit app to focus on chemical management, including a download button for a Test Sheet.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Hi there! Welcome to Streamlit!")
-st.write("My first DataFrame... it's about fish")
-st.dataframe(
-    pd.DataFrame({
-        'Fish': ['Honey Gourami', 'Black Neon Tetra', 'Pygmy Corydora', 'Cherry Shrimp'],
-        'Plants': ['Red Floater', 'Cryptocoryne', 'Monte Carlo', 'Annubias']
-    })
+st.title("Welcome to the Chemical Code of Ethics!")
+st.write("Our goal is to let you manage your own chemicals")
+st.write("To begin, download the Test Sheet and fill in your CAS Numbers and Chemical Names.")
+
+test_list = pd.DataFrame(columns=['name', 'cas_number'])
+csv = test_list.to_csv(index=False)
+
+st.download_button(
+    label="Download Test Sheet",
+    data=csv,
+    file_name="test_list.csv",
+    mime="text/csv"
 )
 
-
-st.write('More importantly... explore my two fish pages')
+st.write("After filling out your sheet please the side bar to navigate to the 'Upload' page.")
