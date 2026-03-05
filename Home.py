a148b46c5146d3fce15a87716193b63186e00cfb
Chemical-COE <cfirkins@unm.edu>
2026-03-04 22:30:17 -0700

    Update and rename app.py to Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,7 @@ import pandas as pd
 st.title("Welcome to the Chemical Code of Ethics App!")
 st.write("Our goal is to let you manage your own chemicals")
 st.write("To begin, download the Test Sheet and fill in your CAS Numbers and Chemical Names.")
+st.write('Your sheet should look similar to the table below. Make sure to retain the original column names ("name" and "cas_number".')
 
 test_list = pd.DataFrame(columns=['name', 'cas_number'])
 csv = test_list.to_csv(index=False)
@@ -19,8 +20,8 @@ st.download_button(
 name = ["Acetone", "Benzene", "Toluene", "Ethanol",]
 cas_number = ["67-64-1", "71-43-2", "108-88-3", "64-17-5"]
 test_list = pd.DataFrame({'name': name, 'cas_number': cas_number})
+
 st.write("After filling out your sheet please use the sidebar to navigate to the 'Upload' page.")
-st.write('Your sheet should look similar to the table below. Make sure to retain the original column names.')
 st.write('You can even choose to download this table as a csv by hovering over the table with your mouse and selecting the download icon.')
 st.dataframe(test_list)
 
