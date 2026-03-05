c467be257dd19e8caf8728a14248f37a1e439932
Chemical-COE <cfirkins@unm.edu>
2026-03-04 21:32:20 -0700

    Update 2_Results.py

--- pages/2_Results.py
+++ pages/2_Results.py
@@ -6,9 +6,14 @@ st.title("Hi there! Welcome to the Results page!")
 sin_list = pd.read_excel('SinList_result.xlsx')
 echa_list = pd.read_excel('chemical_universe_list_en.xlsx')
 
+l = 0
+
 if 'chemical_data' in st.session_state:
     df = st.session_state['chemical_data']
-    st.dataframe(df)
+    st.dataframe(df[['name','cas_number']])
+    l = 1
 
 else:
     st.warning("Please upload your data on the upload page first.")
+
+
