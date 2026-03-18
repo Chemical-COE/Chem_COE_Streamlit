5180ae2cdf0e33ea00111c22b7aa105f64edbc72
Chemical-COE <cfirkins@unm.edu>
2026-03-17 21:55:31 -0600

    Update 1_Mode.py

--- pages/1_Mode.py
+++ pages/1_Mode.py
@@ -8,19 +8,20 @@ if 'mode' not in st.session_state:
     st.session_state['mode'] = 'Default'
 
 st.title("Welcome to the mode page")
-st.write(f"You are currently in {st.session_state['mode']} mode.") ## I need to add this to the Results Page to make sure they are in the right mode.
+
 st.write("Here you can define a few options.")
 st.write("Would you like to use the refrence tables we accesed in September of 2025?")
 st.write("If so press the 'Default' Button Below.")
-
 
 if st.button("Default"):
     st.session_state['mode'] = 'Default'
     
 st.write("Or if would you like to upload your own reffrence tables, please click the 'Advanced' Button bellow")
-st.write(f"You are currently in {st.session_state['mode']} mode.") ## I need to add this to the Results Page to make sure they are in the right mode.
+
 if st.button("Advanced"):
     st.session_state['mode'] = 'Advanced'
+
+st.write(f"You are currently in {st.session_state['mode']} mode.") ## I need to add this to the Results Page to make sure they are in the right mode.
 
 if st.session_state['mode'] == 'Advanced':
     st.write("If you are uploading your own files, please make sure they have the same columns as used in the analysis:")
