832a25f8392f2632a63bb315e5f2cb6a24948e49
Chemical-COE <cfirkins@unm.edu>
2026-03-17 22:40:16 -0600

    Update 4_Stats.py

--- pages/4_Stats.py
+++ pages/4_Stats.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title("Welcome to the Statistics page")
+if st.session_state['result_sucsess'] == 'No_result':
+   st.warning('Please complete your file upload before visiting the statistics page')
+   st.stop()
+  
+if st.session_state['result_sucsess'] == result_passed
+  echa_safe = st.session_state['echa_s']
+  echa_not_safe = st.session_state['echa_ns']
+  sin_safe = st.session_state['sin_s']
+  sin_not_safe = st.session_state['sin_ns']
 
-echa_safe = st.session_state['echa_s']
-echa_not_safe = st.session_state['echa_ns']
-sin_safe = st.session_state['sin_s']
-sin_not_safe = st.session_state['sin_ns']
 
