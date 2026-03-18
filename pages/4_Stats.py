b0d7c8a3a2d8ac687ccebdfd9bfc9aaf6ebd561e
Chemical-COE <cfirkins@unm.edu>
2026-03-17 22:40:56 -0600

    Update 4_Stats.py

--- pages/4_Stats.py
+++ pages/4_Stats.py
@@ -6,7 +6,7 @@ if st.session_state['result_sucsess'] == 'No_result':
    st.warning('Please complete your file upload before visiting the statistics page')
    st.stop()
   
-if st.session_state['result_sucsess'] == result_passed
+if st.session_state['result_sucsess'] == 'result_passed':
   echa_safe = st.session_state['echa_s']
   echa_not_safe = st.session_state['echa_ns']
   sin_safe = st.session_state['sin_s']
