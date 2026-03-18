09856d9b21de55671021291a29eeb0745db4e07c
Chemical-COE <cfirkins@unm.edu>
2026-03-17 22:26:38 -0600

    Update 4_Stats.py

--- pages/4_Stats.py
+++ pages/4_Stats.py
@@ -2,4 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.title("Welcome to the Statistics page")
-st.dataframe(echa_ns)
+
+echa_safe = st.session_state['echa_s']
+echa_not_safe = st.session_state['echa_ns']
+sin_safe = st.session_state['sin_s']
+sin_not_safe = st.session_state['sin_ns']
+
