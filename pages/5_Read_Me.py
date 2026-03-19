8376ad4db4dac6588c44213221e5d5f30fa8b7ae
Chemical-COE <cfirkins@unm.edu>
2026-03-19 15:29:16 -0600

    Update 5_Read_Me.py

--- pages/5_Read_Me.py
+++ pages/5_Read_Me.py
@@ -1,7 +1,7 @@
 # COE-App
 st.title("Welcome to the Read Me.")
 st.write("The goal of this repository is to demonstrate an example suite of tools and resources for companies to understand if they have potentially harmful chemicals in their products and/or supply chains.")
-st.write("We make no claims about the accuracy of the information provided by this app. It was created and tested for demonstration purposes by a small team of researchers, but any company using it should check and revise the code and inputs as they see fit."
+st.write("We make no claims about the accuracy of the information provided by this app. It was created and tested for demonstration purposes by a small team of researchers, but any company using it should check and revise the code and inputs as they see fit.")
 st.write("While this app is mostly functional and deployable, we do not anticipate deploying the app to an app marketplace.")
 st.write("Anyone who is interested in forking this repository for that purpose is encouraged to collaborate and take the app in that future direction.")
 st.write("We only ask that anyone moving forward with this app or new versions of it reference our work, which introduced the app and the ideas behind its creation: XXX(insert paper citation here).")
