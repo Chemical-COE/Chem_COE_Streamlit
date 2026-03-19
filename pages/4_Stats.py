ffcec789d02a22d37a100907a4b48482fa4e4bf7
Chemical-COE <cfirkins@unm.edu>
2026-03-19 15:08:23 -0600

    Update 4_Stats.py

--- pages/4_Stats.py
+++ pages/4_Stats.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 if 'mode' not in st.session_state:
     st.session_state['mode'] = 'Default'
@@ -19,5 +20,16 @@ if st.session_state['result_sucsess'] == 'result_passed':
   sin_safe = st.session_state['sin_s']
   sin_not_safe = st.session_state['sin_ns']
 
+sin_count = len(sin_not_safe)
+safe_count = len(sin_safe)
+
+fig = px.bar(
+    x=['Flagged by SIN List', 'Not Flagged'],
+    y=[sin_count, safe_count],
+    title='SIN List Results',
+    labels={'x': 'Category', 'y': 'Number of Chemicals'},
+    color=['Flagged by SIN List', 'Not Flagged'],
+)
+st.plotly_chart(fig)
 
 
