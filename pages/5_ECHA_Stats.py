7b6d8e15646076780ad688a327e9b873f58aa46f
Chemical-COE <cfirkins@unm.edu>
2026-04-03 09:28:02 -0600

    Update 5_ECHA_Stats.py

--- pages/5_ECHA_Stats.py
+++ pages/5_ECHA_Stats.py
@@ -20,30 +20,26 @@ if st.session_state['result_sucsess'] == 'result_passed':
   sin_safe = st.session_state['sin_s']
   sin_not_safe = st.session_state['sin_ns']
 
-sin_count = len(sin_not_safe)
-safe_count = len(sin_safe)
+echa_count = len(echa_not_safe)
+echa_nr_count = len(echa_safe)
 
 fig = px.bar(
-    x=['Flagged by SIN List', 'Not Flagged'],
-    y=[sin_count, safe_count],
-    title='SIN List Results',
+    x=['Regulated by ECHA List', 'Not Regulated'],
+    y=[echa_count, echa_nr_count],
+    title='ECHA List Results',
     labels={'x': 'Category', 'y': 'Number of Chemicals'},
-    color=['Flagged by SIN List', 'Not Flagged'],
+    color=['Regulated by ECHA List', 'Not Regulated'],
 )
 
-st.write('This reflects the number of chemicals in your test set that are and are not on the SIN List.')
+st.write('This reflects the number of chemicals in your test set that are and are Regulated by the ECHA List.')
 st.plotly_chart(fig)
 
-
-st.write('This reflects the distribution of the SIN Groups in your test list.')
-sin_results_not_safe = sin_not_safe.sort_values('sin_groups', ascending=True).copy()
-fig = px.bar(sin_results_not_safe, x='sin_groups', hover_name = 'test_list_name', hover_data = ['cas_number', 'inclusion_date'], title = 'SIN Group Distribution')
+st.write('This reflects the distribution of the Chemical Universe groups in your test list.')
+echa_results_not_safe = echa_not_safe.sort_values('position_in_the_chemical _universe', ascending=True).copy()
+fig = px.bar(echa_results_not_safe, x='position_in_the_chemical _universe', hover_name = 'test_list_name', hover_data = [ 'cas_number', 'registration_type', 'infocard' ], title = 'ECHA Group Distribution')
 st.plotly_chart(fig)
 
-st.write('This reflects the distribution of Environmental Concerns in your test list.')
-sin_results_not_safe = sin_results_not_safe.sort_values('health_env_concern', ascending=True)
-fig = px.bar(sin_results_not_safe, x='health_env_concern', hover_name='test_list_name', hover_data = ['cas_number', 'inclusion_date'], title = 'Health Envirronmental Concerns')
-st.plotly_chart(fig)
+st.dataframe(echa_not_safef.loc[:, ['cas_number', 'test_list_name','infocard']])
 
 
 
