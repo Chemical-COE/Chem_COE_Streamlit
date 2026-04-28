df1dac4ea04040ab457006157dd66f87a5a81607
Chemical-COE <cfirkins@unm.edu>
2026-04-28 12:17:33 -0600

    Clarify Streamlit data policy statement
    Updated wording in the data policy disclaimer for clarity.

--- pages/6_Read_Me.py
+++ pages/6_Read_Me.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Welcome to the Read Me!")
 st.write("The goal of this repository is to demonstrate an example suite of tools and resources for companies to understand if they have potentially harmful chemicals in their products and/or supply chains.")
-st.write("App creators do not see or store any of your data. However, Streamlit might collect some of your data, which will not be visable to app creators or other users. Streamlit's Data policy can be found here at the button below.")
+st.write("App creators do not see or store any of your data. However, Streamlit might collect some of your data, which will not be visable to app creators or other users. Streamlit's Data policy can be found at the button below.")
 st.link_button("Streamlit Data Policy", "https://streamlit.io/privacy-policy")
 st.write("We make no claims about the accuracy of the information provided by this app. It was created and tested for demonstration purposes by a small team of researchers, but any company using it should check and revise the code and inputs as they see fit.")
 st.write("While this app is mostly functional and deployable, we do not anticipate deploying the app to an app marketplace.")
