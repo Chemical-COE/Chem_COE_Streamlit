39967dbc8d255473dbd937392584d114d7845940
scruggsce <47005606+scruggsce@users.noreply.github.com>
2026-05-07 10:57:54 -0600

    Update 3_Results.py

--- pages/3_Results.py
+++ pages/3_Results.py
@@ -43,8 +43,8 @@ if l == 1:
         test_list['cas_number'] = test_list['cas_number'].astype(str)
     
         # Fill missing data with "NIES" -> Not In Excel Sheet (Missing From Excel Sheet)
-        sin_list.fillna('NIES', inplace=True)
-        echa_list.fillna('NIES', inplace=True)
+        ##sin_list.fillna('NIES', inplace=True)
+        ##echa_list.fillna('NIES', inplace=True)
     
         # Join the test list with the refrerence lists
         echa_results = test_list.merge(echa_list, on='cas_number', how='left')
@@ -68,7 +68,7 @@ if l == 1:
         st.session_state['sin_ns'] = sin_results_not_safe
         
         st.subheader('SIN List Matches: The chemicals in the table below are chemicals you use that are on the SIN List.')
-        st.write("Navigate the results table to understand why each chemical is included in the SIN List, its REACH status, possible safer substitutes, and more.")
+        st.write("Navigate the results table to understand why each chemical is included in the SIN List, its REACH status, ChemSec's suggested marketplace alternatives, and more.")
         st.dataframe(sin_results_not_safe)
   
         st.subheader('REACH Regulation Matches: The chemicals in the table below are chemicals you use that have been registered under the REACH regulation.')
